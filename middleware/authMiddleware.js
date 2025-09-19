27ed62db58cf3d37a5b1b7c043d5ce9aef8d310b
msdharani1 <msdharaniofficial@gmail.com>
2025-09-19 10:08:51 +0530

    add

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,13 @@
+const jwt = require("jsonwebtoken")
 
 const authMiddleware = (req, res, next) => {
     const token = req.cookie.token;
     if(!token){
         return res.status(401).json({message: "Unauthorized/No token"})
     }
-    
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        
+    }
 }
